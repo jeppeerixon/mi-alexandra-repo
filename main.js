315c90f05f56091e8d54fcc2610ff2edd4989e07
Sebastian Post <sebastianpost@protonmail.com>
2023-02-24 10:41:49 +0100

    nav

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 console.log("Hej hej");
-const menuBtn = document.querySelector(".openBtn");
+const openBtn = document.querySelector(".openBtn");
 const closeBtn = document.querySelector(".closeBtn");
 
-menuBtn.addEventListener("click", myFunction);
-closeBtn.addEventListener("click", myFunction);
+openBtn.addEventListener("click", toggleClass);
+closeBtn.addEventListener("click", toggleClass);
 
 
-function myFunction() {
+function toggleClass() {
   const hamburger = document.querySelector(".menu");
   hamburger.classList.toggle("visible");
   closeBtn.classList.toggle("hidden");
-  menuBtn.classList.toggle("hidden");
+  openBtn.classList.toggle("hidden");
   console.log("Klicka");
 } 
